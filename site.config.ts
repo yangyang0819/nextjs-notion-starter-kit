6602f6c9ed6b1dc231984cf3e82e2ed3ff349079
yangyang0819 <112619409+yangyang0819@users.noreply.github.com>
2022-09-01 17:12:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9af9e502581b484596c9b4e40eb58a54',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Privacy Policy ',
+  domain: 'https://wobbly-vision-717.notion.site/Privacy-Policy-9af9e502581b484596c9b4e40eb58a54t',
+  author: 'Space Y',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
